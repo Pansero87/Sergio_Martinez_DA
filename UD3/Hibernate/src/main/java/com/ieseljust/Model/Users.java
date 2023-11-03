74717f6a27ae1cccfb2a306aca37d98df0161b23
SergioMB <prgsergiomb@gmail.com>
2023-11-03 13:02:04 +0100

    Hibernate

---
+++ UD3/Hibernate/src/main/java/com/ieseljust/Model/Users.java
@@ -0,0 +1,59 @@
+package com.ieseljust.Model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    public Users() {
+    }
+
+    public Users(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ UD3/Hibernate/src/main/java/com/ieseljust/SocialNetork/SocialNetwork.java
@@ -0,0 +1,32 @@
+package com.ieseljust.SocialNetork;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.ieseljust.Model.HibernateUtil;
+import com.ieseljust.Model.Users;
+
+public class SocialNetwork {
+    public static void main(String[] args) {
+
+        // Crea una nueva instancia de Users con los datos de la inserción
+        Users user = new Users(1, "Antonio", "2222");
+
+        // Obtiene una sesión de Hibernate
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        // Inicia una transacción
+        Transaction transaction = session.beginTransaction();
+
+        // Guarda el objeto en la base de datos
+        session.save(user);
+
+        // Confirma la transacción
+        transaction.commit();
+
+        // Cierra la sesión
+        session.close();
+
+    }
+
+}
